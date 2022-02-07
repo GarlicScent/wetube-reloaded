60ac9f8e6508e6737ed62cf0942eefa1e7d30792
GarlicScent <dongzai@naver.com>
2022-02-07 17:52:44 +0900

    Using morgan middleware
    Using morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-app.listen(PORT, () => console.log(`Serever Listening on http://localhost:${PORT} 🚀`));
+const handleHome = (req, res) => {
+    return res.send("<h1>I Love Middleware!!</h1>");
+}
+
+const handleLogin = (req, res) => {
+    return res.send("<h1>You are logged in</h1>")
+}
+
+app.use(logger);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () => console.log(`✅ Serever Listening on http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
