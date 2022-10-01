0eae15811e7363a4fb5a9a79125bca3576967f1d
GarlicScent <dongzai@naver.com>
2022-10-01 10:01:52 +0900

    divide server to router and controller, install pug view engine

--- src/server.js
+++ src/server.js
@@ -1,7 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-app.listen(PORT, () => console.log(`Serever Listening on http://localhost:${PORT} 🚀`));
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
+app.listen(PORT, () =>
+	console.log(`Server listening on port http://localhost:${PORT} 🚀`)
+);
