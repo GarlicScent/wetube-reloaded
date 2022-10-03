9fc075b23b1d6c248eea449c0498971846103672
GarlicScent <dongzai@naver.com>
2022-10-03 09:48:04 +0900

    learning about pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,38 @@
-export const trending = (req, res) => res.render("home");
+const fakeUser = {
+	username: "Nico",
+	loggedIn: false,
+};
+const videos = [
+	{
+		title: "first video",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 minutes ago",
+		views: 58,
+		id: 1,
+	},
+	{
+		title: "second video",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 minutes ago",
+		views: 58,
+		id: 1,
+	},
+	{
+		title: "third video",
+		rating: 5,
+		comments: 2,
+		createdAt: "2 minutes ago",
+		views: 58,
+		id: 1,
+	},
+];
+export const trending = (req, res) =>
+	res.render("home", { pageTitle: "Home", fakeUser, videos });
 export const see = (req, res) => {
 	console.log(req.params);
-	return res.send("Watch");
+	res.render("Watch", { pageTitle: "구구까까" });
 };
 export const edit = (req, res) => {
 	console.log(req.params);
