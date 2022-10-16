7852953acc275af2d8b28b7c673f8178b1ee2087
GarlicScent <dongzai@naver.com>
2022-10-17 02:04:48 +0900

    login 만들기(세션 전단계)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
 	return res.render("join", { pageTitle: "Join" });
@@ -23,19 +24,50 @@ export const postJoin = async (req, res) => {
 			errorMessage: "This username/email is already taken",
 		});
 	}
-
-	await User.create({
-		email,
-		username,
-		password,
-		name,
-		location,
-	});
-
-	return res.redirect("/");
+	try {
+		await User.create({
+			email,
+			username,
+			password,
+			name,
+			location,
+		});
+		return res.redirect("/");
+	} catch (error) {
+		return res.status(400).render("join", {
+			pageTitle: "Upload Video",
+			errorMessage: error._message,
+		});
+	}
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => {
+	return res.render("login", { pageTitle: "Login" });
+};
+export const postLogin = async (req, res) => {
+	const { username, password } = req.body;
+	const pageTitle = "Login";
+	//check if account exists
+	const user = await User.findOne({ username });
+	if (!user) {
+		return res.status(400).render("login", {
+			pageTitle,
+			errorMessage: "An account with this username does not exists",
+		});
+	}
+
+	//check if password correct
+	const ok = await bcrypt.compare(password, user.password);
+	//bcrypt에서 사용자가 입력한 비밀번호와 hash된 비밀번호를 비교해서 맞는지 확인한다.
+	if (!ok) {
+		return res.status(400).render("login", {
+			pageTitle,
+			errorMessage: "Wrong password",
+		});
+	}
+	console.log("로그인 접속됨.");
+	return res.redirect("/");
+};
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
