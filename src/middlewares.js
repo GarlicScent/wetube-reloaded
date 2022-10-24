7941980854f95d4c4100492949354e18b6b846ba
GarlicScent <dongzai@naver.com>
2022-10-24 19:01:13 +0900

    #8.0, #8.1 edit profile and add more middleware(로그인된 유저만 접속 가능한 경로, 퍼블릭 경로 접속 가능한 경로 설정)

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,6 +4,21 @@ export const localsMiddleware = (req, res, next) => {
 	// console.log("🚀", req.session.user);
 	res.locals.loggedIn = Boolean(req.session.loggedIn);
 	res.locals.siteName = "Wetube";
-	res.locals.loggedInUser = req.session.user;
+	res.locals.loggedInUser = req.session.user || {};
 	next();
+};
+
+export const protectMiddleware = (req, res, next) => {
+	if (req.session.loggedIn) {
+		next();
+	} else {
+		return res.redirect("/login");
+	}
+};
+export const publicOnlyMiddleware = (req, res, next) => {
+	if (!req.session.loggedIn) {
+		next();
+	} else {
+		return res.redirect("/");
+	}
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,20 +3,26 @@ import {
 	edit,
 	finishGithubLogin,
 	finishKakaoLogin,
+	getEdit,
 	logout,
+	postEdit,
 	see,
 	startGithubLogin,
 	startKakaoLogin,
 } from "../controllers/userController";
+import { protectMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
-userRouter.get("/:id(\\d+)", see);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectMiddleware, logout);
+userRouter.route("/edit").all(protectMiddleware).get(getEdit).post(postEdit);
 
-userRouter.get("/kakao/start", startKakaoLogin);
-userRouter.get("/kakao/finish", finishKakaoLogin);
+//github REST api login, oauth2.0
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+//kakao REST api login, oauth2.0
+userRouter.get("/kakao/start", publicOnlyMiddleware, startKakaoLogin);
+userRouter.get("/kakao/finish", publicOnlyMiddleware, finishKakaoLogin);
+
+userRouter.get("/:id(\\d+)", see);
 export default userRouter;
