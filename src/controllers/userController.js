8a62fb3d6ba815d762c34c6e2315a8a986ce82e3
GarlicScent <dongzai@naver.com>
2022-10-31 14:57:17 +0900

    #8.10 user profile. (유저 프로파일 정보 페이지 추가)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -362,4 +362,14 @@ export const postChangePassword = async (req, res) => {
 	await user.save();
 	res.redirect("/users/logout");
 };
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+	const { id } = req.params;
+	const user = await User.findById(id);
+	if (!user) {
+		return res.status(404).render("404", { pageTitle: "User Not Found" });
+	}
+	return res.render("users/profile", {
+		pageTitle: `${user.name}'s Profile`,
+		user,
+	});
+};
