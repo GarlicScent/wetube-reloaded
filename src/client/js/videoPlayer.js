64d6a329d4eb017ea2e6c584b3b8ebfcf949dbe7
GarlicScent <dongzai@naver.com>
2022-11-19 10:40:20 +0900

    #11.6 Fullscreen. fullscreen api로 비디오를 전체화면으로 바꿔주기

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,3 +1,4 @@
+const videoContainer = document.getElementById("videoContainer");
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
@@ -5,6 +6,7 @@ const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
 const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreenBtn");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -65,9 +67,23 @@ const handleTimeLineChange = (event) => {
 	video.currentTime = value;
 };
 
+const handleFullScreen = () => {
+	const fullscreen = document.fullscreenElement;
+	if (fullscreen) {
+		fullScreenBtn.innerText = "Exit Full Screen";
+		document.exitFullscreen();
+	} else {
+		videoContainer.requestFullscreen();
+		fullScreenBtn.innerText = "Enter Full Screen";
+		//video.requestFullscreen(); 하면 비디오만 풀화면되고, 콘트롤러들은 안보이게된다.
+		//videoContainer로 원하는 요소들을 감싼다음에 풀스크린해줘야된다.
+	}
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimeLineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
