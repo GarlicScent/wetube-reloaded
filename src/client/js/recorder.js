210b3e6b1beed80811e7bca3e37fcca7c245c029
GarlicScent <dongzai@naver.com>
2022-11-28 00:11:50 +0900

    #13.1 Video Preview. 비디오 녹화를 위한 확인용 영상 보는 것 만들기.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,14 @@
 const startBtn = document.getElementById("startBtn");
-
+const video = document.getElementById("preview");
 const handleStart = async () => {
 	const constraints = {
-		audio: true,
-		video: true,
+		audio: false,
+		video: true, //{ width: 200, height: 100 }
 	};
 	const stream = await navigator.mediaDevices.getUserMedia(constraints);
 	console.log(stream);
+	video.srcObject = stream;
+	video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
