28b14772c17dacf032e23c9d70b51aa8278a841a
GarlicScent <dongzai@naver.com>
2022-12-01 15:40:50 +0900

    #13.4 Downloading the File. 녹화하고 저장까지 완료~!

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,19 @@
+const { Body } = require("node-fetch");
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+	const a = document.createElement("a");
+	a.href = videoFile;
+	a.download = "MyRecording.webm";
+	document.body.appendChild(a);
+	a.click();
+};
 
 const handleStop = () => {
 	startBtn.innerText = "Download Recording";
@@ -19,8 +28,8 @@ const handleStart = () => {
 	startBtn.addEventListener("click", handleStop);
 
 	recorder = new MediaRecorder(stream);
-	recorder.ondataavailable = (e) => {
-		const videoFile = URL.createObjectURL(e.data);
+	recorder.ondataavailable = (event) => {
+		videoFile = URL.createObjectURL(event.data);
 		//.createObjectURL()은 브라우저 메모리에서만 사용 가능한 url을 생성해준다. 이 url은 파일을 가르킨다. 브라우저 yes. server no!
 		video.srcObject = null;
 		video.src = videoFile;
