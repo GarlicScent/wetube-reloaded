53f8a6ec554997576a6a326984407fae6a544a57
GarlicScent <dongzai@naver.com>
2022-12-15 13:57:09 +0900

    #16.7 Realtime Comments. window.location을 계속 reload 하지 않고 화면에 바로 추가되게끔 보이게 프론트에서 설정

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,21 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const addComment = (text) => {
+	const videoComments = document.querySelector(".video__comments ul");
+	const newComment = document.createElement("li");
+	newComment.className = "video__comment";
+	const icon = document.createElement("i");
+	icon.className = "fas fa-comment";
+	const span = document.createElement("span");
+	span.innerText = ` ${text}`;
+	newComment.appendChild(icon);
+	newComment.appendChild(span);
+
+	videoComments.prepend(newComment);
+};
+
+const handleSubmit = async (event) => {
 	event.preventDefault();
 	const textarea = form.querySelector("textarea");
 	const text = textarea.value;
@@ -9,7 +23,7 @@ const handleSubmit = (event) => {
 	if (text === "") {
 		return;
 	}
-	fetch(`/api/videos/${videoId}/comment`, {
+	const { status } = await fetch(`/api/videos/${videoId}/comment`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
@@ -17,6 +31,11 @@ const handleSubmit = (event) => {
 		//headers 설정으로 json을 보내는 것을 알려주지 않으면 express.json()이 그냥 string을 보내는 것으로 안다.
 		body: JSON.stringify({ text }),
 	});
+
+	if (status === 201) {
+		console.log("create fake comment");
+		addComment(text);
+	}
 	textarea.value = "";
 };
 
